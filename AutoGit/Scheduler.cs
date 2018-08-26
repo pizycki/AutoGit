b0f976ff8b2556ea6ac3c44f16793c182c1f0bac
Paweł Iżycki <pawelizycki@gmail.com>
2018-08-26 21:32:01 +0200

    Scheduler

--- AutoGit/Scheduler.cs
+++ AutoGit/Scheduler.cs
@@ -1,9 +1,39 @@
 ﻿using System;
+using System.Linq.Expressions;
+using Hangfire;
+using Hangfire.MemoryStorage;
 
 namespace AutoGit
 {
-    public class Scheduler
+    public class Scheduler : IDisposable
     {
-         
+        private readonly BackgroundJobServer _server;
+
+        private Scheduler()
+        {
+            _server = new BackgroundJobServer();
+        }
+
+        public void AddCronJob(Expression<Action> methodCall, Func<string> cron)
+        {
+            RecurringJob.AddOrUpdate(methodCall, cron);
+        }
+
+        public void Dispose()
+        {
+            _server?.Dispose();
+        }
+
+        public static Scheduler Create()
+        {
+            if (!storageAddedToConfiguration)
+            {
+                GlobalConfiguration.Configuration.UseMemoryStorage();
+            }
+
+            return new Scheduler();
+        }
+
+        private static bool storageAddedToConfiguration = false;
     }
 }
