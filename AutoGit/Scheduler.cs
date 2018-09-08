cf5b570eb1fbd318887dea9e7cdeab69ef2d25cb
Paweł Iżycki <pawelizycki@gmail.com>
2018-09-08 20:16:03 +0200

    GetRemaningTimeToNextRun

--- AutoGit/Scheduler.cs
+++ AutoGit/Scheduler.cs
@@ -3,6 +3,7 @@ using System.Linq.Expressions;
 using AutoGit.Contracts;
 using Hangfire;
 using Hangfire.MemoryStorage;
+using Hangfire.Storage;
 
 namespace AutoGit
 {
@@ -18,6 +19,21 @@ namespace AutoGit
         public void AddCronJob(Expression<Action> methodCall, Func<string> cron)
         {
             RecurringJob.AddOrUpdate(methodCall, cron);
+        }
+
+        public TimeSpan GetRemaningTimeToNextRun(string jobName)
+        {
+            using (var connection = JobStorage.Current.GetConnection())
+            {
+                var job = connection.GetRecurringJobs().Find(j => j.Id.Contains(jobName));
+
+                if (job.NextExecution.HasValue)
+                {
+                    return DateTime.Now - job.NextExecution.Value;
+                }
+
+                return default;
+            }
         }
 
         public void Dispose()
