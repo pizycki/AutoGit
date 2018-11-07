d1d2fe3bbcce3b7a1751e5d722f2d914003e8927
Paweł Iżycki <pawelizycki@gmail.com>
2018-11-07 17:49:57 +0100

    Fsharp impl

---
+++ AutoGit.Fsharp/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+open Argu
+open System.Diagnostics
+
+module AutoGit =
+    type Arguments =
+        | Interval of interval:int
+        | Working_Directory of dir:string
+        | Push
+    
+    let Loop interval work =
+        while true do
+            let async = async {
+                printfn "Wait for %i" interval
+                do! Async.Sleep interval
+                printfn "Do some work"
+                work()
+            }
+            Async.RunSynchronously async
+            
+
+    let PrintStatus () =
+        use p = new Process()
+        p.StartInfo <- ProcessStartInfo("git", "status")
+        p.Start() |> ignore
+        ()
+        
+
+[<EntryPoint>]
+let main argv =
+    printfn "=== AutoGit started ==="
+    let seconds = 5 * 1000
+    AutoGit.Loop seconds AutoGit.PrintStatus
+    0 // return an integer exit code
