8c0b7bcde15695137bc280d96e1befcec7a54afe
Paweł Iżycki <pawel.izycki@britishcouncil.pl>
2018-11-10 20:54:04 +0100

    Looks cool, but we need sth diff

--- AutoGit.Fsharp/Program.fs
+++ AutoGit.Fsharp/Program.fs
@@ -2,33 +2,85 @@
 open Argu
 open System.Diagnostics
 
-module AutoGit =
-    type Arguments =
-        | Interval of interval:int
-        | Working_Directory of dir:string
-        | Push
+module Git =
     
-    let Loop interval work =
-        while true do
-            let async = async {
-                printfn "Wait for %i" interval
-                do! Async.Sleep interval
-                printfn "Do some work"
-                work()
-            }
-            Async.RunSynchronously async
-            
+    type GitArgName = string
+    type GitArgValue = string
+    type GitArg = GitArgName * GitArgValue
+    type GitRepository = string  
 
-    let PrintStatus () =
+    let directoryArg (repo:GitRepository option): GitArg option =
+        match repo with
+        | Some r -> Some ("-C", r)
+        | _ -> None
+    
+//    let commitAll 
+
+    let printStatus () =
         use p = new Process()
         p.StartInfo <- ProcessStartInfo("git", "status")
         p.Start() |> ignore
-        ()
-        
+        ()   
 
+open Git
+module AutoGit =
+    type Arguments =
+        | Interval of int
+        | Directory of dir:string
+        | Push of bool
+    with
+        interface IArgParserTemplate with
+            member arg.Usage =
+                match arg with
+                | Interval _ -> "Commit interval in minutes"
+                | Directory _ -> "Path to Git repository directory"
+                | Push _ -> "Pushes changes to remote with each commit"
+    
+    type CommitInterval = TimeSpan  
+
+    type AutoCommitArguments = {
+        Interval: CommitInterval
+        Repository: GitRepository option
+        Push: bool
+    }
+
+    let loop interval work =
+        while true do
+            let async = async {
+                do! Async.Sleep interval
+                work()
+            }
+            Async.RunSynchronously async
+
+    let convert (args:AutoCommitArguments) = 
+        let gitArgs = [
+            directoryArg args.Repository;
+        ]
+
+        let f a = 
+            match a with 
+            | Some x -> [fst x; snd x]
+            | None -> []
+
+        List.collect f gitArgs
+
+open AutoGit
 [<EntryPoint>]
 let main argv =
-    printfn "=== AutoGit started ==="
-    let seconds = 5 * 1000
-    AutoGit.Loop seconds AutoGit.PrintStatus
+    let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
+    let parser = ArgumentParser.Create<Arguments>(programName = "ls", errorHandler = errorHandler)     
+    let results = parser.ParseCommandLine argv
+
+    let interval = match results.TryGetResult<@Arguments.Interval@> with Some v -> v | None -> 3600
+    let repository = results.TryGetResult<@Directory@> 
+    let push = match results.TryGetResult<@Arguments.Push@> with Some v -> v | None -> false
+    
+    let commitArgs = {
+        Interval = TimeSpan.FromSeconds (float interval);
+        Repository = repository;
+        Push = push
+    }
+
+    printfn "Bla %A" (convert commitArgs |> List.toArray)
+
     0 // return an integer exit code
